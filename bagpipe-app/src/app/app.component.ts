0bd7682bb5cc6207846f8473887f300c4cc9452b
Alex Sandridge <alsa6908@colorado.edu>
2019-06-02 16:57:34 -0600

    Added CLI app

---
+++ bagpipe-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  tab : number = 0;
+
+  changeTab(tab){
+    this.tab = tab;
+  }
+}
